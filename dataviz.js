1c9d58dbd924aa2dc593ed5e4fad83d7ff585af3
Ben Krepp <bkrepp@ctps.org>
2020-05-08 16:20:49 -0400

    Reorganized code.

--- dataviz.js
+++ dataviz.js
@@ -1,54 +1,11 @@
+// Data stores for TMC and speed data read in
 tmc_data = [];
 speed_data = [];
 
-var tmc_row_converter = function(d) {
-	var FOO = 0;
-	return {
-		tmc : 		d.tmc,
-		seq_num:	+d.seq_num,
-		from_meas:	parseFloat(d.from_meas),
-		distance:	parseFloat(d.distance),
-		spd_limit:	+d.spd_limit
-	};
-};
-
-
-d3.csv("data/i93_nb_tmcs.csv", function(d) {
-	return {
-		tmc : 		d.tmc,
-		seq_num:	+d.seq_num,
-		from_meas:	parseFloat(d.from_meas),
-		distance:	parseFloat(d.distance),
-		spd_limit:	+d.spd_limit
-	};
-}).then(function(data) {
-	tmc_data = data;
-});
-
-// Format of INRIX timestamp is yyyy-mm-dd hh:mm:ss
-// Note: space between 'dd' and 'mm'.
-// Return object with hour and minute, both as integers.
-//
-function get_time_from_timestamp(tstamp) {
-	hms = tstamp.split(' ')[1].split(':');
-	hr = +hms[0];
-	min = +hms[1];
-	return { 'hr' : hr, 'min' : min };
-}
-
-d3.csv("data/i93_nb_2020-03-13.csv", function(d) {
-	return {
-		tmc : 	d.tmc,
-		time: 	get_time_from_timestamp(d.tstamp),
-		speed:	parseFloat(d.speed)
-	};
-}).then(function(data) {
-	speed_data = data;
-});
-
+// Globals used to generate the data viz
 var cell_w = 10,
     cell_h = 10;
-	
+
 var recs_per_hour = 6;
 var time_recs = recs_per_hour * 24;
 var minutes_per_rec = 10;
@@ -62,7 +19,22 @@ var w = cell_w * time_recs,
 var speed_scale = d3.scaleThreshold()
 					.domain([10, 20, 30, 40, 50, Infinity])
 					.range(['#cc1414', '#ff4719', '#ffa319', '#ffe019', '#affc19', '#32fa32']); 
-	
+
+// Utility function used when parsing speed data
+//
+// Format of INRIX timestamp is yyyy-mm-dd hh:mm:ss
+// Note: space between 'dd' and 'mm'.
+// Return object with hour and minute, both as integers.
+//
+function get_time_from_timestamp(tstamp) {
+	hms = tstamp.split(' ')[1].split(':');
+	hr = +hms[0];
+	min = +hms[1];
+	return { 'hr' : hr, 'min' : min };
+}
+
+// Function to generate the data viz.
+//
 function generate_viz() {
 	var svg = d3.select("#viz_div")
 				.append("svg")
@@ -92,10 +64,10 @@ function generate_viz() {
 				temp = speed_scale(d.speed);
 				// Tests for missing speed value in input data - scale function couldn't
 				if (typeof temp === 'undefined') { 
-					console.log('tmc = ' + d.tmc)
-					console.log('hour = ' + d.time['hr']);
-					console.log('minute = ' + d.time['min']);
-					console.log('scale function returned: ' + temp);
+					// console.log('tmc = ' + d.tmc)
+					// console.log('hour = ' + d.time['hr']);
+					// console.log('minute = ' + d.time['min']);
+					// console.log('scale function returned: ' + temp);
 					retval = "gray";
 				} else {
 					retval = temp;
@@ -103,3 +75,34 @@ function generate_viz() {
 				return retval;
 		})
 } //  generate_viz()
+
+// Execution starts here:
+// 		Kick things off by loading the TMC data;
+// 		when this has completed, load the speed data;
+// 		when that has completed, generate the viz.
+//
+d3.csv("data/i93_nb_tmcs.csv", function(d) {
+	return {
+		tmc : 		d.tmc,
+		seq_num:	+d.seq_num,
+		from_meas:	parseFloat(d.from_meas),
+		distance:	parseFloat(d.distance),
+		spd_limit:	+d.spd_limit
+	};
+}).then(function(data) {
+	tmc_data = data;
+	
+	// Now load the speed data
+	d3.csv("data/i93_nb_2020-03-13.csv", function(d) {
+		return {
+			tmc : 	d.tmc,
+			time: 	get_time_from_timestamp(d.tstamp),
+			speed:	parseFloat(d.speed)
+		};
+	}).then(function(data) {
+		speed_data = data;
+		
+		// Now, generate the viz
+		generate_viz();
+	})
+});
