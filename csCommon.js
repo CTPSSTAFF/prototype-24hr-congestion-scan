937cde6757242d8f8398929cae26a18d44af7dc5
Ben Krepp <bkrepp@ctps.org>
2020-08-14 15:24:12 -0400

    Moved get_{speed,speed_index} into library.

--- csCommon.js
+++ csCommon.js
@@ -5,13 +5,18 @@
 (function() {
     // Internals of the library (if any) go here
     //
+	// Data value to indicate either:
+	//     1. No speed data value present in input data
+	//     2. Speed data value present in input data, but cvalue less than "default" minimum cvalue (i.e., 75.0).
+	var _NO_DATA =	-9999;
+	
 	// *** Public API of the library begins here. ***
 	return csCommon = {
 		version : "1.0",
 		// Data value to indicate either:
 		//     1. No speed data value present in input data
 		//     2. Speed data value present in input data, but cvalue less than "default" minimum cvalue (i.e., 75.0).
-		NO_DATA :	-9999,
+		NO_DATA :	_NO_DATA,
 		
 		// Minium cvalue of data records used to generate visualization.
 		// By default, this value is 75.0, the minimum cvalue of data records used in CMP analyses.
@@ -37,6 +42,56 @@
 
 		speed_index_legend_labels : ['No Data', '0.4', '0.5', '0.7', '0.9', '>0.9'],
 		
+		// get_speed - Function to 'safely' parse and return speed value.
+		//
+		// Speed data may be missing in some records.
+		// When this is the case record this explicitly with the NO_DATA value,
+		// so scale and legend functions can work w/o requiring hacks.
+		// We also do not use records with a cvalue less than 75.0, setting
+		// their value to NO_DATA for purposes of generating the visualization.
+		//
+		get_speed	: function(d, min_cvalue) {
+							// var temp_str = 'Entering get_speed. TMC = ' + d.tmc;
+							var retval, speed, cvalue;
+							speed = parseFloat(d.speed);
+							cvalue = parseFloat(d.cvalue);
+							if (isNaN(cvalue)) {
+								// Ugh... we have to test for this also...
+								var _DEBUG_HOOK = 0;
+							}
+							if (isNaN(speed) || cvalue < min_cvalue) {
+								retval = _NO_DATA;
+								// console.log('Mapping ' + temp + ' to NO_DATA.');
+							} else {
+								retval = speed;
+							}
+							// console.log(temp_str + ' retval = ' + retval);
+							return retval;
+					},	// get_speed()
+		
+		// #2 - Function to 'safely' parse speed and spd_limit values, 
+		//      and compute and return speed index.
+		// See comments on preceeding function.
+		//
+		get_speed_index : function(d, min_cvalue) {
+							var retval, speed, cvalue, tmc_rec, spd_limit;
+							speed = parseFloat(d.speed);
+							cvalue = parseFloat(d.cvalue);
+							if (isNaN(cvalue)) {
+								// Ugh... we have to test for this also...
+								var _DEBUG_HOOK = 0;
+							}
+							if (isNaN(speed) || cvalue < min_cvalue) {
+								retval = _NO_DATA;
+							} else {
+								tmc_rec = _.find(tmc_data, function(rec) { return rec.tmc == d.tmc; });
+								spd_limit = tmc_rec['spd_limit'];
+								temp = speed / spd_limit;
+								retval = temp;
+							}
+							return retval;
+						},
+
 		// get_time_from_timestamp: Parse an INRIX timestamp, and return object with 'hr' and 'min' properties
 		// Format of INRIX timestamp is yyyy-mm-dd hh:mm:ss
 		// Note: space between 'dd' and 'mm'.
