866dd5d0ea6a421e19ed862fae360d477ec1452b
Ben Krepp <bkrepp@ctps.org>
2020-10-06 10:32:54 -0400

    Data coverage extended to September 30, 2020.

--- dataviz3.js
+++ dataviz3.js
@@ -12,7 +12,7 @@ var tmc_data = [],
     speed_data = [];
 var current_route = 'i93_nb',
 	first_date = '2020-01-01';
-	last_date = '2020-08-31',
+	last_date = '2020-09-30',
 	default_start_date = '2020-03-01',
 	default_end_date = '2020-06-30';
 
